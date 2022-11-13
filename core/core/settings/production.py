07b30853c0b9eb1439e3258c6bdef7778cbb2ba6
Diego Asencio <diegoasencio96@gmail.com>
2022-11-13 13:21:49 -0500

    add base project wagtail

---
+++ core/core/settings/production.py
@@ -0,0 +1,26 @@
+DEBUG = False
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/dbname',
+        # for django-redis < 3.8.0, use:
+        # 'LOCATION': '127.0.0.1:6379',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    },
+    'renditions': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 600,
+        'OPTIONS': {
+            'MAX_ENTRIES': 1000
+        }
+    }
+}
+
+try:
+    from .local import *
+except ImportError:
+    pass
